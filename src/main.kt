cdf1550e59302fcf1e66987d5db878c179abb2cb
paulndungu <p.ndungu.kara@gmail.com>
2020-02-15 15:26:39 +0300

    Dont worry if again this one looks familiar to the inhritance

---
+++ src/main.kt
@@ -0,0 +1,53 @@
+import java.lang.annotation.Inherited
+
+open class sum(){
+    fun sum(n1:Int , n2:Int, n3:Int): Int{
+        return n1+n2+n3
+    }
+    fun sub(n1:Int,n2: Int,n3: Int): Int {
+        return n1-n2-n3
+    }
+}
+//----------------------------------------------------------------------------------------------------------------------
+class multipurpose(): sum() {
+    fun div(n1:Float,n2:Float):Float{
+        return n1/n2
+    }
+    fun prod(n1:Int,n2:Int):Int{
+        return n1*n2
+    }
+    fun brack(n1:Int,n2:Int,n3:Int,n4:Int):Int{
+        return (n1+n2)-(n3+n4)
+    }
+}
+
+fun main(){
+
+    var sum= sum()
+    var add=sum.sum(11,12,13)
+    println("Addition: $add")
+
+    val sub= sum()
+    var subract=sub.sub(11,12,13)
+    println("Subtarction= $subract")
+
+    println("------------------------------------")
+//    ------------------------------------------------------------------------------------------------------------------
+    val div= multipurpose()
+    var over=div.div(21f,22f)
+    println("Division: $over")
+
+    val prod= multipurpose()
+    var multiply=prod.prod(21,22)
+    println("Multiplication: $multiply")
+
+    val brack= multipurpose()
+    var brackets=brack.brack(21,22,23,24)
+    println("Brackets: $brackets")
+//    ------------------------------------------------------
+    val insum=sum()
+    var insum2:Int?
+    insum2=brack.sum(11,12,13)
+    println("Inherited sum: $insum2")
+
+}
